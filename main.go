42916aa8a4835feef2a60b4743fd6b2715abf749
barbaros affan yildirim <barbarosayildirim@gmail.com>
2024-02-05 00:42:02 +0300

    feat: add scraping menu dates

--- main.go
+++ main.go
@@ -1,9 +1,44 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/chromedp/chromedp"
 )
 
+func getData() {
+
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	var htmlContent string
+	abuzer := chromedp.Run(ctx,
+		chromedp.Navigate(`https://sks.iuc.edu.tr/tr/yemeklistesi`),
+		chromedp.WaitVisible(`#tab-ogle`, chromedp.ByQuery),
+		chromedp.OuterHTML(`html`, &htmlContent, chromedp.ByQuery),
+	)
+	if abuzer != nil {
+		log.Fatal(abuzer)
+	}
+
+	doc, ilayda := goquery.NewDocumentFromReader(strings.NewReader(htmlContent))
+	if ilayda != nil {
+		log.Fatal(ilayda)
+	}
+
+	doc.Find("#tab-ogle section.monu-container > table > tbody > tr > td > b").Each(func(i int, s *goquery.Selection) {
+		fmt.Println("Found: ", s.Text(), "on Index: ", i)
+	})
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	getData()
 }
